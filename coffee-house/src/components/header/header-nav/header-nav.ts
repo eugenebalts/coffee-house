4ea196099499217c3dc5965b2679f29c541357b0
Yauheni Baltsevich <norcobeatz@gmail.com>
2023-11-24 23:22:41 +0100

    feat: create header navigation component

---
+++ coffee-house/src/components/header/header-nav/header-nav.ts
@@ -0,0 +1,43 @@
+import BaseComponent from '../../base-component';
+import navigationItems from './navigation-items';
+import Link from '../../link/link';
+import { INavigationItems } from '../../../types/types';
+import './header-nav.scss';
+
+export default class HeaderNav extends BaseComponent<'nav'> {
+  constructor() {
+    super('nav', ['header__nav']);
+
+    this.node.append(this.createMarkup());
+  }
+
+  private createMarkup() {
+    const navigationList = new BaseComponent('ul', [
+      'header__nav-list',
+    ]).getNode();
+
+    for (const navItem of navigationItems) {
+      const navigationItem = this.createNavItem(navItem, ['header__nav-item']);
+
+      navigationList.append(navigationItem);
+    }
+
+    return navigationList;
+  }
+
+  private createNavItem(item: INavigationItems, classNames: string[]) {
+    const navigationItem: HTMLLIElement = new BaseComponent('li', [
+      ...classNames,
+    ]).getNode();
+
+    const itemLink: HTMLAnchorElement = new Link(
+      item.title,
+      ['header__link'],
+      item.href
+    ).getNode();
+
+    navigationItem.append(itemLink);
+
+    return navigationItem;
+  }
+}
