1b763a5c4da4508ae5b4276d91bff59a01e8a604
Yauheni Baltsevich <norcobeatz@gmail.com>
2023-11-27 01:09:41 +0100

    feat: create section-application

--- coffee-house/src/pages/home-page/home-page.ts
+++ coffee-house/src/pages/home-page/home-page.ts
@@ -4,6 +4,7 @@ import './home-page.scss';
 import '../pages.scss';
 import SectionFavorite from './section-favorite/section-favorite';
 import SectionAbout from './section-about/section-about';
+import SectionApp from './section-application/section-app';
 
 export default class HomePage extends BaseComponent<'div'> {
   constructor() {
@@ -18,8 +19,9 @@ export default class HomePage extends BaseComponent<'div'> {
     const sectionHero = new SectionHero().getNode();
     const sectionFavorite = new SectionFavorite().getNode();
     const sectionAbout = new SectionAbout().getNode();
+    const sectionApp = new SectionApp().getNode();
 
-    wrapper.append(sectionHero, sectionFavorite, sectionAbout);
+    wrapper.append(sectionHero, sectionFavorite, sectionAbout, sectionApp);
 
     return wrapper;
   }
