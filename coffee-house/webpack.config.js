fb64d3bafbd714a3c3dfe1590cc234ab466b1d16
Yauheni Baltsevich <norcobeatz@gmail.com>
2023-12-05 21:39:34 +0100

    chore: configure source-map

--- coffee-house/webpack.config.js
+++ coffee-house/webpack.config.js
@@ -2,8 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
-  devtool: 'source-map',
+  devtool: 'inline-source-map',
   entry: './src/index.ts',
   module: {
     rules: [
